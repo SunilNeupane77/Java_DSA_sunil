ee74e6a1a615ba2f5510970873eeaa6f5014e427
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-06-29 21:17:02 +0545

    sorting selection

---
+++ Sorting/selectionsort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class selectionsort {
+    public static void main(String[] args) {
+        int[] array = { 64, 25, 12, 22, 11 };
+        selectionSort(array);
+        System.out.println("Sorted array:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
