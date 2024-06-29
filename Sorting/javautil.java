0499c61f9d7f4816e22bedc4b065c76626bc0bdf
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-06-29 22:16:31 +0545

    Sorting in java.util package

---
+++ Sorting/javautil.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class javautil {
+    public static void main(String[] args) {
+        int[] numbers = { 5, 2, 8, 3, 1 };
+
+        // Sorting the array
+        Arrays.sort(numbers);
+
+        // Printing the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(numbers));
+    }
+}
